ddfa95f8472ca1ef6a6e25b2ad0f60d90c80e36d
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2023-06-12 22:25:11 -0700

    Update volume mean square data check program with latest modules

--- src/programs/data_utilities/TIME_HISTORIES/compare_sph_mean_square.f90
+++ src/programs/data_utilities/TIME_HISTORIES/compare_sph_mean_square.f90
@@ -15,122 +15,139 @@
 !
       use m_precision
       use skip_comment_f
+      use t_buffer_4_gzip
+      use t_read_sph_spectra
+      use t_sph_spectr_head_labels
+!
+      use compare_sph_monitor_header
+      use sel_gz_input_sph_mtr_head
+      use select_gz_stream_file_IO
+      use gz_spl_sph_spectr_data_IO
 !
       implicit none
 !
+      character(len = kchara) :: fname_rms_vol
+      character(len = kchara) :: fname_rms_ref
 !
-      integer(kind = kint) :: ierr, i, icomp, itmp, istep, istep_ref
-      integer(kind = kint) :: nfld, ltr_sph
-      integer(kind = kint) :: ICB_id, CMB_id
-      integer(kind = kint) :: kr_in, kr_out
-      real(kind = kreal) :: r_in, r_out
-      character(len=255) :: tmpchara
 !
-      character(len = kchara) :: fname_rms_vol = 'sph_pwr_volume_s.dat'
-      character(len = kchara) :: fname_rms_ref                          &
-     &                               = 'reference/sph_pwr_volume_s.dat'
-      integer(kind = kint), parameter :: id_file_rms =      34
+      integer(kind = kint), parameter :: id_file1 = 34, id_file2 = 36
+      character, pointer :: FPz_f1, FPz_f2
+      type(buffer_4_gzip) :: zbuf1, zbuf2
+      type(read_sph_spectr_data) :: sph_IN1, sph_IN2
+      type(sph_spectr_head_labels) :: sph_lbl_IN1, sph_lbl_IN2
+      logical :: flag_gzip1, flag_gzip2
+      logical :: flag_miss1, flag_miss2
+      character(len = kchara) :: file_name
 !
-      integer(kind = kint) :: ntot_sph_spec
-      character(len = kchara), allocatable :: ene_sph_spec_name(:)
-      real(kind = kreal), allocatable :: spectr_t(:)
-      real(kind = kreal), allocatable :: spectr_ref(:)
+      logical :: error
+      integer(kind = kint) :: ierr1, ierr2
       real(kind = kreal) :: diff
-      real(kind = kreal) :: time, time_ref
-!
-      open (id_file_rms,file=fname_rms_vol)
-!
-      read(id_file_rms,*)
-      read(id_file_rms,*) itmp, ltr_sph
-      read(id_file_rms,*)
-      read(id_file_rms,*) ICB_id, CMB_id
-      read(id_file_rms,*)
-      read(id_file_rms,*) kr_in, r_in
-      read(id_file_rms,*)
-      read(id_file_rms,*) kr_out, r_out
-      read(id_file_rms,*)
-      read(id_file_rms,*) nfld, ntot_sph_spec
-!
-      allocate( ene_sph_spec_name(ntot_sph_spec) )
-      allocate( spectr_t(ntot_sph_spec) )
-      allocate( spectr_ref(ntot_sph_spec) )
-!
-!    Evaluate time average
-!
-      istep = 0
-      read(id_file_rms,*) tmpchara, tmpchara,                           &
-     &                    ene_sph_spec_name(1:ntot_sph_spec)
-      write(*,'(26a1,a6,i12,a8)',advance="NO") (char(8),i=1,26),        &
-     &       'step= ', istep,   ' is read'
-      do
-        read(id_file_rms,*,err=99,end=99) istep, time,                  &
-     &         spectr_t(1:ntot_sph_spec)
-        write(*,'(26a1,a6,i12,a8)',advance="NO") (char(8),i=1,26),      &
-     &       'step= ', istep,   ' is read'
-      end do
-!
-  99  continue
-      close(id_file_rms)
-!
-      open (id_file_rms,file=fname_rms_ref)
-!
-      read(id_file_rms,*)
-      read(id_file_rms,*) itmp, ltr_sph
-      read(id_file_rms,*)
-      read(id_file_rms,*) ICB_id, CMB_id
-      read(id_file_rms,*)
-      read(id_file_rms,*) kr_in, r_in
-      read(id_file_rms,*)
-      read(id_file_rms,*) kr_out, r_out
-      read(id_file_rms,*)
-      read(id_file_rms,*) nfld, ntot_sph_spec
-!
-!    Evaluate time average
-!
-      istep_ref = 0
-      read(id_file_rms,*) tmpchara, tmpchara,                           &
-     &                    ene_sph_spec_name(1:ntot_sph_spec)
-      write(*,'(26a1,a6,i12,a8)',advance="NO") (char(8),i=1,26),        &
-     &       'step= ', istep_ref,   ' is read'
-      do
-        read(id_file_rms,*,err=98,end=98) istep_ref, time_ref,          &
-     &         spectr_ref(1:ntot_sph_spec)
-        write(*,'(26a1,a6,i12,a8)',advance="NO") (char(8),i=1,26),      &
-     &       'step= ', istep_ref,   ' is read'
-      end do
-      write(*,*) char(10)
-!
-  98  continue
-      close(id_file_rms)
+      real(kind = kreal), allocatable :: spectr_IN1(:)
+      real(kind = kreal), allocatable :: spectr_IN2(:)
+      integer(kind = kint) :: icomp, icomp2
 !
 !
-      ierr = 0
-      if(istep .ne. istep_ref) then
-          write(*,*) 'time step error'
+      if(iargc_kemo() .le. 1) then
+        write(*,*)                                                      &
+     &     'kemo_module_dep REFERENCE_FILE_NAME COMPARED_FILE_NAME'
+        stop
       end if
+      call getarg_k(1, fname_rms_ref)
+      call getarg_k(2, fname_rms_vol)
+      write(*,*) 'reference file name: ', trim(fname_rms_ref)
+      write(*,*) 'comparerd file name: ', trim(fname_rms_vol)
 !
-      diff = compare_data(time, time_ref)
-      if(diff .gt. 1.d-9) then
-          write(*,*) 'Large error in time: ',                           &
-     &                time, time_ref, diff
-      end if
-
-      do icomp = 1, ntot_sph_spec
-        diff = compare_data(spectr_t(icomp), spectr_ref(icomp))
-        if(diff .gt. 1.d-9) then
-          write(*,*) 'Large error in ', trim(ene_sph_spec_name(icomp)), &
-     &           ': ', spectr_t(icomp), spectr_ref(icomp), diff
-          ierr = 1
-          exit
-        end if
-      end do
-
-      if(ierr .gt. 0) then
-        write(*,*) '!!!!!!  Chack Failed!!'
+!
+      call sel_open_check_gz_stream_file(FPz_f1, id_file1,              &
+     &   fname_rms_vol, flag_gzip1, flag_miss1, file_name, zbuf1)
+      call read_sph_volume_mean_head(FPz_f1, id_file1, flag_gzip1,      &
+     &                               sph_lbl_IN1, sph_IN1, zbuf1)
+!
+      call sel_open_check_gz_stream_file(FPz_f2, id_file2,              &
+     &   fname_rms_ref, flag_gzip2, flag_miss2, file_name, zbuf2)
+      call read_sph_volume_mean_head(FPz_f2, id_file2, flag_gzip2,      &
+     &                               sph_lbl_IN2, sph_IN2, zbuf2)
+!
+      error = .not. cmp_sph_volume_monitor_heads                        &
+     &            (sph_lbl_IN1, sph_IN1, sph_lbl_IN2, sph_IN2)
+      if(error) then
+        write(*,*) 'time sequence data header does not match'
         stop 1
       end if
 !
-      write(*,*) 'Mean square data are successfully checked'
+      allocate(spectr_IN1(sph_IN1%ntot_sph_spec))
+      allocate(spectr_IN2(sph_IN2%ntot_sph_spec))
+!
+      do
+        call gz_read_volume_pwr_sph(FPz_f1, id_file1, flag_gzip1,       &
+     &      sph_IN1%ntot_sph_spec, sph_IN1%i_step, sph_IN1%time,        &
+     &      spectr_IN1(1), zbuf1, ierr1)
+        call gz_read_volume_pwr_sph(FPz_f2, id_file2, flag_gzip2,       &
+     &      sph_IN2%ntot_sph_spec, sph_IN2%i_step, sph_IN2%time,        &
+     &      spectr_IN2(1), zbuf2, ierr2)
+        if(ierr1*ierr2 .gt. 0) exit
+        if(ierr1+ierr1 .gt. 0 .and. ierr1*ierr2 .eq. 0) then
+          error = .TRUE.
+          exit
+        end if
+!
+        error = .FALSE.
+        do icomp = 1, sph_IN1%ntot_sph_spec
+          diff = compare_data(spectr_IN1(icomp), spectr_IN2(icomp))
+          if(abs(diff) .gt. 1.d-9) then
+            icomp2 = icomp + sph_IN1%num_time_labels
+            write(*,*) 'Large error in ',                               &
+     &           trim(sph_IN1%ene_sph_spec_name(icomp2)),               &
+     &           ': ', spectr_IN1(icomp), spectr_IN2(icomp), diff
+            error = .TRUE.
+            exit
+          end if
+        end do
+      end do
+!
+      call sel_close_read_gz_stream_file                                &
+     &   (FPz_f1, id_file1, flag_gzip1, zbuf1)
+      call sel_close_read_gz_stream_file                                &
+     &   (FPz_f2, id_file2, flag_gzip2, zbuf2)
+!
+      if(error) then
+        write(*,*) 'time sequence data does not match'
+        stop 1
+      end if
+!
       stop 0
 !
+!   --------------------------------------------------------------------
+!
+      contains
+!
+!   --------------------------------------------------------------------
+!
+      subroutine getarg_k(i, argc)
+!
+      integer, intent(in) :: i
+      character(len=*), intent(out) :: argc
+!
+      call getarg(0, argc)
+      if(argc == "") then
+        call getarg(i + 1, argc)
+      else
+        call getarg(i, argc)
+      end if
+      end subroutine getarg_k
+!
+!   --------------------------------------------------------------------
+!
+      integer function iargc_kemo() result(oresult)
+!
+      integer :: iargc
+      character(len=8) :: argc
+      oresult = iargc()
+      call getarg(0, argc)
+      if(argc == "") then
+        oresult = oresult - 1
+      end if
+      end function iargc_kemo
+!
+!   --------------------------------------------------------------------
       end program compare_sph_mean_square
