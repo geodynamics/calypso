764cad95fc6d1d9988a9451a935ea690ad4c1922
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-10-10 11:31:19 +0900

    Update controls and README file in dynamo benchmark case 0 example

---
+++ examples/dynamo_benchmark/dynamobench_case_0/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+#
+echo 'Clean results'
+rm -f monitor/[A-Q,S-Z,a-q,s-z]*
+rm -f rst_6/[A-Q,S-Z,a-q,s-z]*
+rm -f field/[A-Q,S-Z,a-q,s-z]*
+rm -f sections/[A-Q,S-Z,a-q,s-z]*
+rm -f *.dat *~ controls/*~
