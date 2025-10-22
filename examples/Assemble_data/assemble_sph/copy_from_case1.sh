68bd0f156a730ec366c90a7389c9a7fe112f94e9
Hiroaki Matsui <hrmatsui@ucdavis.edu>
2025-10-22 13:07:28 +0900

    Add examples

---
+++ examples/Assemble_data/assemble_sph/copy_from_case1.sh
@@ -0,0 +1,6 @@
+#
+#  Shell script to copy data from Benchmark case 1
+#
+cp ../dynamo_benchmark/dynamobench_case_1/sph_lm31r48c_4/* sph_lm31r48c_4/
+cp ../dynamo_benchmark/dynamobench_case_1/rst_4/rst.* 4domains/
+
